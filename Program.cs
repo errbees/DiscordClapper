306db635e30dcbc9538ebe0a2ba3280b014a400c
errbees <42676809+errbees@users.noreply.github.com>
2019-03-14 20:07:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DiscordClapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string output = "";
+
+            char[] input = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    output += " :clap: ";
+                }
+                else output += input[i];
+            }
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+    }
+}
